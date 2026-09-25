d2304fc70d018e5ecbc63c946fd4491657e1881f
Maxim Deyev <dm.041@yahoo.com>
2026-09-25 14:36:37 +0300

    new baseurl

---
+++ build/assets/js/65bb7f9c.d2e3c545.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs_website=self.webpackChunkdocs_website||[]).push([["504"],{6144(e){e.exports=JSON.parse('{"metadata":{"permalink":"/docusaurus/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ build/assets/js/acecf23e.8bf9349b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs_website=self.webpackChunkdocs_website||[]).push([["220"],{1912(s){s.exports=JSON.parse('{"blogBasePath":"/docusaurus/blog","blogTitle":"Blog","authorsListPath":"/docusaurus/blog/authors"}')}}]);
